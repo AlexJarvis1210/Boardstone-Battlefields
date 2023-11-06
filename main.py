9559c02ea6510a7f289ee2b37193eda7355db2fe
AlexJarvis1210 <113143815+AlexJarvis1210@users.noreply.github.com>
2023-11-06 16:54:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pygame
 from game import Game
 
-if __name__ == '__main__':
-    game_instance = Game()
-    game_instance.run()
+# Initialize Pygame
+pygame.init()
+
+# Set up the display
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Boardstone Battlefields')
+
+# Create the game object
+game = Game(screen)
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update game logic and draw the board
+    game.update()
+    game.draw()
+
+    # Update the display
+    pygame.display.flip()
+
+# Clean up Pygame
+pygame.quit()
